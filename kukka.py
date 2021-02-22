a777fcf57df31f3160ee9713d1263692aeb4ec39
Aleksi Myllyoja <aleksi.myllyoja@gmail.com>
2021-02-22 15:45:39 +0200

    plants

--- kukka.py
+++ kukka.py
@@ -7,6 +7,8 @@ from scipy import ndimage
 import sys
 from random import *
 
+_take2 = lambda ps: list(map(lambda p: [p[0], p[1]], ps))
+
 def bezier(ps, n=100):
     def _bpns(ps, n=100):
         return np.array(
@@ -14,9 +16,6 @@ def bezier(ps, n=100):
              for i in range(0, len(ps))
             ]
         )
-
-    def _take2(ps):
-        return list(map(lambda p: [p[0], p[1]], ps))
 
     def _bernstein(i, n, t):
         return comb(n, i)*(t**(n-i))*(1-t)**i
@@ -51,7 +50,7 @@ def create_image(width=300, height=218, s=2):
     img[:,:] = (255, 255, 255)
     return img
 
-def show_image(image, rotation=90):
+def show_image(image, rotation=-90):
     cv2.imshow(
         'image',
         ndimage.rotate(image, rotation)
@@ -93,38 +92,171 @@ def _circle(x, y, radiuses, angles):
         )
     return ps
 
-def shaky_line(p1, p2):
+def line_variations(p1, p2, vs):
     a = line_angle(p1, p2)
-    splits = np.linspace(0, 1, 5)
+    splits = np.linspace(0, 1, len(vs)+2)
     bps = []
     for i, s in enumerate(splits[1:-1]):
         p = split_on(p1, p2, s)
-        #mark(*p1, image, size=1)
-        bps.append(
-            circle_point(*p, randint(0, 10), a+(-1)**i*pi/2)
-        )
+        pn = circle_point(*p, vs[i], a+pi/2)
+#        mark(*pn, image, size=1)
+        bps.append(pn)
 
-    #mark(*p1, image, size=2)
-    #mark(*p2, image, size=2)
+#    mark(*p1, image, size=2)
+#    mark(*p2, image, size=2)
 
     bps.insert(0, p1)
     bps.append(p2)
 
-    return bezier(bps, n=10)
+    return bps
 
-image = create_image()
+def shaky_line(p1, p2, variations, bp=10):
+    bps = line_variations(p1, p2, variations)
+    return bezier(bps, n=bp)
 
-precision = 20
-radiuses = [randint(10, 90) for x in range(precision-1)]
-radiuses.append(radiuses[0])
-angles = np.linspace(0, 2*pi, precision)
+def _c0():
+    precision = 4
+    radiuses = [10+x*10 for x in range(int(precision/2))]
+    radiuses += reversed(radiuses)
 
-c1 = _circle(150, 109, radiuses, angles)
-cps = list(zip(c1, c1[1:]))
+    angles = np.linspace(0, 2*pi, precision)
 
-c2 = [shaky_line(p1, p2) for (p1, p2) in cps]
+    c1 = _circle(150, 109, radiuses, angles)
+    cps = list(zip(c1, c1[1:]))
 
-paths = c2
-image = plot_paths(paths, image)
+    #variations = [randint(-50, 50) for i in range(10)]
 
-show_image(image)
+    return [
+        shaky_line(p1, p2, [randint(-50, 50) for i in range(10)], bp=10)
+        for (p1, p2) in cps
+    ]
+
+flatten = lambda t: [item for sublist in t for item in sublist]
+
+_R = lambda a: np.array(
+    [[cos(a), -sin(a), 0],
+     [sin(a), cos(a),  0],
+     [0,      0,       1]]
+)
+
+_T = lambda x, y: np.array(
+    [[1, 0, x],
+     [0, 1, y],
+     [0, 0, 1]]
+)
+
+_HS = lambda s: np.array(
+    [[1, s, 0],
+     [0, 1, 0],
+     [0, 0, 1]]
+)
+
+_VS = lambda s: np.array(
+    [[1, 0, 0],
+     [s, 1, 0],
+     [0, 0, 1]]
+)
+
+rotate_around = lambda ps, p0, a: _take2(list(map(
+    lambda p: _T(*p0)@_R(a)@_T(-p0[0], -p0[1])@(p+[1]),
+    ps
+)))
+
+shear_around = lambda ps, p0, s: _take2(list(map(
+    lambda p: _T(*p0)@_VS(s)@_T(-p0[0], -p0[1])@(p+[1]),
+    ps
+)))
+
+def leaf(p1, p2, rotation, shear_amount):
+    global image
+
+    #mark(*p1, image, size=5)
+
+    d = distance(p1, p2)
+
+    base_variations1 = [randint(0, 30-i*6) for i in range(5)]
+    base_variations2 = list(map(lambda x: -x, base_variations1))
+
+    sl1 = bezier(line_variations(p1, p2, base_variations1), n=6)
+    sl2 = bezier(line_variations(p1, p2, base_variations2), n=6)
+
+    sl1 = shear_around(sl1, p1, shear_amount)
+    sl2 = shear_around(sl2, p1, shear_amount)
+
+    sl1 = rotate_around(sl1, p1, rotation)
+    sl2 = rotate_around(sl2, p1, rotation)
+
+    #image = plot_paths([list(reversed(sl1))+sl2], image, color=(0, 0, 255))
+
+    vi = int(d/7)
+
+    vs2 = [randint(-vi, vi) for i in range(10)]
+    vs3 = [randint(-vi, vi) for i in range(10)]
+
+    cps1 = list(zip(sl1, sl1[1:]))
+    cps2 = list(zip(sl2, sl2[1:]))
+
+    l1 = [shaky_line(p1, p2, vs2, 100) for (p1, p2) in reversed(cps1)]
+    l2 = [shaky_line(p1, p2, vs3, 100) for (p1, p2) in reversed(cps2)]
+
+    return list(reversed(flatten(l2)))+flatten(l1)
+
+def stem(p1, p2):
+
+    base_variations = [randint(-20, 20) for i in range(5)]
+    base = bezier(line_variations(p1, p2, base_variations), n=6)
+
+    return base
+
+#paths = _c0()
+#paths = leaf((50, 109), (100, 109))
+
+def create_plant():
+    image = create_image()
+
+    paths = []
+    _stem = stem((50, 109), (280, 109))
+
+    paths.append(_stem)
+    #mark(*stem[1], image, size=5)
+
+    for i in range(1, 6):
+        paths.append(
+            leaf(
+                _stem[i], (_stem[i][0], _stem[i][1]-(80-i*10)),
+                rotation=pi/randint(4, 6),
+                shear_amount=random()
+            )
+        )
+
+        paths.append(
+            leaf(
+                _stem[i], (_stem[i][0], _stem[i][1]+(80-i*10)),
+                rotation=-pi/randint(4, 6),
+                shear_amount=random()
+            )
+        )
+
+    image = plot_paths(paths, image)
+
+    return image
+
+import matplotlib.pyplot as plt
+
+w=2
+h=2
+fig=plt.figure(figsize=(8, 8))
+columns = 2
+rows = 2
+for i in range(1, columns*rows +1):
+
+    img = create_plant()
+    img = cv2.transpose(img)
+
+    fig.add_subplot(rows, columns, i)
+    plt.imshow(img)
+    plt.axis('off')
+
+plt.show()
+
+#show_image(image)
